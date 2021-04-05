3bea35db77b22b79cdd9a145512e5443d8c8025b
leezzx <79442805+leezzx@users.noreply.github.com>
2021-04-05 22:25:30 +0900

    Add java file [JDBC 5]

---
+++ Notice.java
@@ -0,0 +1,74 @@
+package com.newlecture.app.entity;
+
+import java.util.Date;
+
+public class Notice {
+	
+	private int id;
+	private String title;
+	private String writerId;
+	private Date regDate;
+	private String content;
+	private int hit;
+	private String files;
+	
+	public Notice() {
+		
+	}
+	
+	public Notice(int id, String title, String writerId, Date regDate, String content, int hit, String files) { // 우클릭 -> source -> generate constructor using fields
+		this.id = id;
+		this.title = title;
+		this.writerId = writerId;
+		this.regDate = regDate;
+		this.content = content;
+		this.hit = hit;
+		this.files = files;
+	}
+
+	public int getId() { // 우클릭 -> source -> generate getter and setters -> sellect all
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriterId() {
+		return writerId;
+	}
+	public void setWriterId(String writerId) {
+		this.writerId = writerId;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+
+	public String getFiles() {
+		return files;
+	}
+
+	public void setFiles(String files) {
+		this.files = files;
+	}
+	
+}
